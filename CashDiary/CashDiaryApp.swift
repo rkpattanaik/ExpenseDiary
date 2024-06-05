7a67d6b2581816376f5d990bdc556aa1792ce48e
Rajesh Pattanaik <rkpattanaik21@gmail.com>
2024-06-06 01:43:52 +0530

    Initial Commit

---
+++ CashDiary/CashDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashDiaryApp.swift
+//  CashDiary
+//
+//  Created by Rajesh Pattanaik on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CashDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
